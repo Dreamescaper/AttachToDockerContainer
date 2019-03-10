d6bc55d84a0b40261d778e0edc2c7ba1ef22e004
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2019-03-10 13:51:45 +0200

    WIP

---
+++ AttachToDockerContainerDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+
+namespace AttachToDockerContainer
+{
+    public partial class AttachToDockerContainerDialog : DialogWindow
+    {
+        public AttachToDockerContainerDialog()
+        {
+            InitializeComponent();
+            ContainerComboBox.ItemsSource = GetContainerNames();
+        }
+
+        private string[] GetContainerNames()
+        {
+
+        }
+    }
+}
